94415a7f87800b927075f86c79389c3aa204be38
starlordsmom <ashaitar@yahoo.com>
2020-10-12 18:01:28 +0200

    processs ne_structures on top level

--- try_this.py
+++ try_this.py
@@ -8,7 +8,6 @@ __author__ = "Anastassia Shaitarova"
 # import xml.etree.ElementTree as ET
 from lxml import etree
 from pathlib import Path
-import re
 
 outpath = Path('output/')
 outpath.mkdir(parents=True, exist_ok=True)
@@ -17,26 +16,13 @@ article = format('test.xml')
 
 tree = etree.parse(article)
 
-pattern = re.compile('neg_structure\\[(.)')
-
-
-# counts the number of parents to the root element
-def get_depth(element):
-    # from https://stackoverflow.com/questions/39112938/parse-hierarchical-xml-tags
-    depth = 0
-    parent = element.getparent()
-    path = [parent.tag]
-    while parent is not None and not str(parent.tag).startswith('sentence'):
-        # print(parent.tag)
-        depth += 1
-        parent = parent.getparent()
-        if parent is not None:
-            path.append(parent.tag)
-
-    return path
-
 
 def process_sentence(sentence, i, negation_events):
+
+    tokens = []
+    cues = []
+    scope = []
+
     negs = 0
 
     try:
@@ -45,87 +31,98 @@ def process_sentence(sentence, i, negation_events):
             # top level neg_structures
             if element.tag == 'neg_structure':
                 negs += 1
-                print('A', negs)
-                for el_in_neg_structure in list(element):
+                if negs == i:
 
-                    if el_in_neg_structure.tag == 'scope':
-                        # print('*** beginning of scope ***')
-                        for el_in_scope in list(el_in_neg_structure):
+                    for el_in_neg_structure in list(element):
 
-                            # if cue is a multiword it has attribute 'discid="1n/c"'
-                            if el_in_scope.tag == 'negexp' and el_in_scope.attrib:
-                                for neg in list(el_in_scope):
-                                    pass
+                        if el_in_neg_structure.tag == 'scope':
+                            for el_in_scope in list(el_in_neg_structure):
 
-                            # regular cue has no attribute
-                            elif el_in_scope.tag == 'negexp':
-                                for neg in list(el_in_scope):
-                                    path = neg.getroottree().getpath(neg)
-                                    paths = path.split('/')
-                                    if pattern.search(path):
-                                        ns = pattern.search(path).group(1)
-                                    if negs == i:
-                                        print(get_depth(neg)[::-1])
-                                        print(negation_events)
+                                # if cue is a multiword it has attribute 'discid="1n/c"'
+                                if el_in_scope.tag == 'negexp' and el_in_scope.attrib:
+                                    for neg in list(el_in_scope):
+                                        print('A', negs)
+                                        tokens.append(neg.get('wd').upper())
+                                        cues.append(2)
+                                        scope.append(0)
 
-                            elif el_in_scope.tag == 'neg_structure':
-                                negs += 1
-                                print('C', negs)
-                                for neg in list(el_in_scope):
-                                    if neg.tag == 'scope':
-                                        for s in list(neg):
-                                            if s.tag == 'negexp':
-                                                for n in list(s):
-                                                    if n.tag == 'event':
-                                                        for e in list(n):
-                                                            if negs == i:
-                                                                # path = e.getroottree().getpath(e)
-                                                                # paths = path.split('/')
-                                                                # print(paths)
-                                                                print(get_depth(e)[::-1])
-                                                                print(negation_events)
+                                # regular cue has no attribute
+                                elif el_in_scope.tag == 'negexp':
+                                    for neg in list(el_in_scope):
+                                        tokens.append(neg.get('wd').upper())
+                                        cues.append(1)
+                                        scope.append(0)
 
-                        # process nested neg_structure
-                    elif el_in_neg_structure.tag == 'neg_structure':
-                        negs += 1
-                        print('B', negs, i)
-                        for subsection in list(el_in_neg_structure):
-                            if subsection.tag == 'scope':
-                                for el_in_scope in list(subsection):
-
-                                    # if cue is a multiword it has attribute 'discid="1n/c"'
-                                    if el_in_scope.tag == 'negexp' and el_in_scope.attrib:
-                                        for neg in list(el_in_scope):
+                                else:
+                                    for neg in el_in_scope.iter():
+                                        try:
+                                            if neg.get('wd'):
+                                                # print(neg.get('wd'))
+                                                tokens.append(neg.get('wd'))
+                                                cues.append(3)
+                                                scope.append(1)
+                                        except:
                                             pass
 
-                                    # regular cue has no attribute
-                                    elif el_in_scope.tag == 'negexp':
-                                        for neg in list(el_in_scope):
-                                            if negs == i:
-                                                # path = neg.getroottree().getpath(neg)
-                                                # paths = path.split('/')
-                                                # print(paths)
-                                                print(get_depth(neg)[::-1])
-                                                print(negation_events)
+                        elif el_in_neg_structure.tag == 'negexp' and el_in_neg_structure.attrib:
+                            for neg in list(el_in_neg_structure):
+                                tokens.append(neg.get('wd').upper())
+                                cues.append(2)
+                                scope.append(0)
+
+                        # regular cue has no attribute
+                        elif el_in_neg_structure.tag == 'negexp':
+                            for neg in list(el_in_neg_structure):
+                                tokens.append(neg.get('wd').upper())
+                                cues.append(1)
+                                scope.append(0)
 
                         else:
+                            for el in el_in_neg_structure.iter():
+                                try:
+                                    if el.get('wd'):
+                                        tokens.append(el.get('wd'))
+                                        cues.append(3)
+                                        scope.append(0)
+                                except:
+                                    pass
+
+                else:
+                    for el_in_neg_structure in element.iter():
+                        try:
+                            if el_in_neg_structure.get('wd'):
+                                tokens.append(el_in_neg_structure.get('wd'))
+                                cues.append(3)
+                                scope.append(0)
+                        except:
                             pass
 
             # collect other words in the sentence
             elif element.get('wd'):
-                pass
-                # print(element.get('wd'))
+                tokens.append(element.get('wd'))
+                cues.append(3)
+                scope.append(0)
 
     except TypeError:
         pass
+
+    return (tokens, cues, scope)
 
 
 for sentence in tree.findall('.//sentence'):
     elems = sentence.xpath('//negexp')
     negation_events = sum(1 for x in sentence.iter('neg_structure'))
     print(negation_events)
+    reslist = list(sentence.iter())
+    text_tokens = [x.get('wd') for x in reslist if x.get('wd')]
+    text = ' '.join([x.get('wd') for x in reslist if x.get('wd')])
+    print(len(text_tokens), text)
 
     for i in range(negation_events):
-        process_sentence(sentence, i + 1, negation_events)
+        tokens, cues, scope = process_sentence(sentence, i + 1, negation_events)
+        print(len(tokens), ' '.join(tokens))
+        print(cues)
+        print(scope)
+        print()
 
     print('***************************')
