c28c14fe4836884587956f5924e6fe2dea5c2fb7
starlordsmom <ashaitar@yahoo.com>
2020-11-13 13:22:05 +0100

    final version, produces specific anno needed for prodigy

--- cardboard4prodigy.py
+++ cardboard4prodigy.py
@@ -11,10 +11,11 @@ import spacy
 from xml.etree import ElementTree as ET
 from bs4 import BeautifulSoup as BS
 from spacy.tokens import Doc
+import jsonlines
 
 
-anno = "test_sherlock.json"
-# "output/SHERLOCK_cardbord.json"
+# anno = "test_sherlock.json"
+anno = "output/SHERLOCK_cardbord.json"
 parallels = "corpora/box.en.ru.txt"
 
 rusent = re.compile(r'>(.*)<')
@@ -36,57 +37,67 @@ class WhitespaceTokenizer(object):
 nlp.tokenizer = WhitespaceTokenizer(nlp.vocab)
 
 
-with open(parallels, 'r') as p, open(anno, 'r') as a:
+with open(parallels, 'r') as p, open(anno, 'r') as a, jsonlines.open('output.jsonl', mode='w') as writer:
     data = json.load(a)
     for line in p:
         for sent in data:
             spans = []
-            scopes = {}
 
             if ' '.join(sent[0]) in line and 0 not in sent[1]:
                 line = line.strip().split('\t')
                 mysent = ET.fromstring(line[0]).text
                 doc = nlp(mysent)
-                scope_tokens = []
 
-                print(mysent)
-                print(len(mysent.split()))
+                cues = {}
+                cue_chunk = []
 
-                for cue in sent[1]:
-                    if cue != 3:
-                        cues = {}
-                        print(sent[1].index(cue))
-                        print('LOOK HERE', doc[sent[1].index(cue)])
-                        onset = doc[sent[1].index(cue)].idx
-                        offset = onset + len(doc[sent[1].index(cue)].text)-1
+                for ind, cue in enumerate(sent[1]):
+                    if cue == 3 and len(cue_chunk) != 0:
+                        onset = doc[cue_chunk[0]].idx
+                        offset = doc[cue_chunk[-1]].idx + len(doc[cue_chunk[-1]].text)
                         cues["start"] = onset
                         cues["end"] = offset
                         cues["label"] = "CUE"
                         spans.append(cues)
+                        cues = {}
+                        cue_chunk = []
+                    elif cue != 3:
+                        cue_chunk.append(ind)
+
+                scopes = {}
+                scope_chunk = []
 
                 for ind, token in enumerate(sent[2]):
-                    if token != 0:
-                        scope_tokens.append(ind)
-
-                begin = doc[scope_tokens[0]].idx
-                end = doc[scope_tokens[-1]].idx + len(doc[scope_tokens[-1]].text)-1
-                scopes["start"] = begin
-                scopes["end"] = end
-                scopes["label"] = "SCOPE"
-                spans.append(scopes)
-                print(scope_tokens)
+                    if token == 0 and len(scope_chunk) != 0:
+                        begin = doc[scope_chunk[0]].idx
+                        end = doc[scope_chunk[-1]].idx + len(doc[scope_chunk[-1]].text)
+                        scopes["start"] = begin
+                        scopes["end"] = end
+                        scopes["label"] = "SCOPE"
+                        spans.append(scopes)
+                        scope_chunk = []
+                        scopes = {}
+                    elif token != 0:
+                        scope_chunk.append(ind)
 
                 soup = BS(line[1], features="html.parser")
                 russian = ''
                 for s in soup.find_all('s'):
                     russian += s.text + ' '
-                print(russian)
 
-                print({
+                # "spans": [{"start": 93, "end": 120, "label": "SCOPE"}, {"start": 121, "end": 123, "label": "CUE"},{"start": 124, "end": 132, "label": "SCOPE"}]}]}
+                spans = sorted(spans, key=lambda start: start['start'])
+
+                print(({
+                    "text": russian,
+                    "view_id": "ner_manual",
+                    "versions": [{"text": mysent,
+                                  "spans": spans}]
+                }))
+
+                writer.write({
                     "text": russian,
                     "view_id": "ner_manual",
                     "versions": [{"text": mysent,
                                   "spans": spans}]
                 })
-
-                print()
